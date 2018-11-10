3255dac371d6e1c63c0b3ab6cdf18379d3bea78a
Galibier <cx.sjtu@gmail.com>
2018-11-10 15:44:11 +0800

    linkedList

--- 06_LinkedList/GeneralSingleList/singleList.h
+++ 06_LinkedList/GeneralSingleList/singleList.h
@@ -1,8 +1,57 @@
-//
-// Created by cxsjt on 11/10/2018.
-//
-
 #ifndef DATASTRUCTURE_SINGLELIST_H
 #define DATASTRUCTURE_SINGLELIST_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct listNode {
+    struct listNode *next;
+    void *value;
+} listNode;
+
+typedef struct linkedList {
+    listNode *head;
+    size_t len;
+    size_t typeSize;
+
+    void (*dup)(void *, void *);
+
+    int (*match)(void *, void *);
+
+    void (*free)(void *);
+} linkedList;
+
+#define listSetDupMethod(l, m) ((l)->dup = (m))
+#define listSetFreeMethod(l, m) ((l)->free = (m))
+#define listSetMatchMethod(l, m) ((l)->match = (m))
+
+#define listGetDupMethod(l) ((l)->dup)
+#define listGetFreeMethod(l) ((l)->free)
+#define listGetMatchMethod(l) ((l)->match)
+
+linkedList *listCreate();
+
+void listRelease(linkedList *list);
+
+void listEmpty(linkedList *list);
+
+linkedList *listAddNodeHead(linkedList *list, void *value);
+
+linkedList *listAddNodeTail(linkedList *list, void *value);
+
+linkedList *listInsertNode(linkedList *list, listNode *oldNode, void *value, bool after);
+
+void listDelNode(linkedList *list, ListNode *node);
+
+
+listNode *listSearchKey(linkedList *list, void *key);
+
+ListNode *listIndex(linkedList *list, long index);
+
+linkedList *listRewind(linkedList *list);
+
+
+size_t listLength(linkedList *list);
+
 
 #endif //DATASTRUCTURE_SINGLELIST_H
